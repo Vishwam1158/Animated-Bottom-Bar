d8e55ab0d6999ab84b1f55c015f9f20e430f0beb
Vishwam1158 <vishwam1158@gmail.com>
2024-06-20 12:34:58 +0530

    add CustomBottomNavigation

---
+++ app/src/main/java/com/viz/animatedbottombar/CustomBottoNavigation.kt
@@ -0,0 +1,115 @@
+package com.viz.animatedbottombar
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun CustomBottomNavigation() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .height(80.dp)
+            .paint(
+                painter = painterResource(R.drawable.bottom_navigation),
+                contentScale = ContentScale.FillHeight
+            )
+            .padding(horizontal = 40.dp)
+    ) {
+        listOf( Icons.Filled.Home,Icons.Filled.Call).map { image ->
+            IconButton(onClick = { }) {
+                Icon(imageVector = image, contentDescription = null, tint = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
+fun AnimatedFab(
+    modifier: Modifier,
+    icon: ImageVector? = null,
+    opacity: Float = 1f,
+    backgroundColor: Color = MaterialTheme.colorScheme.secondary,
+    onClick: () -> Unit = {}
+) {
+    FloatingActionButton(
+        onClick = onClick,
+//        containerColor = FloatingActionButtonDefaults.containerColor,
+        elevation = FloatingActionButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp),
+        modifier = modifier.scale(1.25f) //.background(backgroundColor).
+    ) {
+        icon?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = null,
+                tint = Color.White.copy(alpha = opacity)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun FabGroup(
+    animationProgress: Float = 0f,
+    renderEffect: androidx.compose.ui.graphics.RenderEffect? = null,
+    toggleAnimation: () -> Unit = { }
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .graphicsLayer { this.renderEffect = renderEffect }
+            .padding(bottom = 44.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+
+        AnimatedFab(
+            icon = Icons.Default.FavoriteBorder,
+            modifier = Modifier
+                .rotate(
+                    255 * FastOutSlowInEasing.transform(0.1f, 1.0f, animationProgress)
+                ),
+            opacity = LinearEasing.transform(0.2f, 0.7f, animationProgress)
+        )
+
+
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+private fun CustomBottomNavigationPreview() {
+    CustomBottomNavigation()
+    FabGroup()
+
+}
