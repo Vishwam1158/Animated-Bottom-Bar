c44e65058ce7c31aac834e3cc16cb4e687772e5b
Vishwam1158 <vishwam1158@gmail.com>
2024-06-20 12:32:12 +0530

    add Colors

--- app/src/main/java/com/viz/animatedbottombar/ui/theme/Color.kt
+++ app/src/main/java/com/viz/animatedbottombar/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.viz.animatedbottombar.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple200 = Color(0xFFBB86FC)
+val Purple700 = Color(0xFF3700B3)
+val Pink = Color(0xFFEB87CE)
+val DarkPurple = Color(0xFF3A2F6E)
